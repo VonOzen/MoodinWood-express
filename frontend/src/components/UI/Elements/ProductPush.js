b3ec52615c5569ecd0d016e4593373e0160183b7
VonOzen <hello@simonescure.fr>
2020-08-11 16:43:30 +0200

    Init Landing Page integration

---
+++ frontend/src/components/UI/Elements/ProductPush.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProductPush = () => {
+
+  return (
+    <h2 className="u-h2">Les créations</h2>
+  )
+}
+
+export default ProductPush
--- frontend/src/components/pages/LandingPage.js
+++ frontend/src/components/pages/LandingPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import HeroBanner from '../UI/Common/HeroBanner'
+import ProductPush from '../UI/Elements/ProductPush'
+import Section from '../UI/Common/Section'
 
 const LandingPage = () => {
   return (
     <>
       <HeroBanner />
+      <Section id="miw-creations">
+        <ProductPush />
+      </Section>
     </>
   )
 }
